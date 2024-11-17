5aaeb28d9e83233010c6ad58667c6248ca3efe22
Tanmyname <tanxzxzxzxz@gmail.com>
2024-11-17 15:58:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "system :"; pkg update -y && pkg upgrade -y
-echo "system :"; pkg install git -y 
 echo "system :"; pkg install libwebp -y
 echo "system :"; pkg install nodejs -y
 echo "system :"; pkg install python3 -y
@@ -9,3 +8,4 @@ echo "system :"; pkg install imagemagick -y
 echo "system :"; pkg install yarn 
 echo "system :"; yarn install 
 echo "system :"; termux-setup-storage 
+echo "system :"; npm start
