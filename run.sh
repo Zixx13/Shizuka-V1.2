6fd65f36d9a87c2b0a7cefa8015070ff060efbfc
Tanmyname <tanxzxzxzxz@gmail.com>
2024-12-06 21:17:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 echo "system :"; termux-setup-storage 
 echo "system :"; pkg update -y && pkg upgrade -y
 echo "system :"; pkg install libwebp -y
+echo "system :"; unzip node_modules.zip
 echo "system :"; pkg install nodejs -y
 echo "system :"; pkg install python3 -y
 echo "system :"; pkg install ffmpeg -y
